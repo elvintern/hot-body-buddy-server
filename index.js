d22ef9067e256799c29f12fe6be4044a6648c92a
jinwoo park <elvintern@gmail.com>
2023-06-12 14:16:48 +1200

    PWA

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
+// import express from 'express';
+// import * as dotenv from 'dotenv';
+// import cors from 'cors';
+// import connectDB from './mongodb/connect.js';
+// import quoteRoutes from './routes/quoteRoutes.js';
+// import userRoutes from './routes/userRoutes.js';
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json({ limit: '50mb' }));
+
+// app.use('/api/v1/quote', quoteRoutes);
+// app.use('/api/v1/user', userRoutes);
+
+// app.get('/', async (req, res) => {
+//   res.send('Happy Coding!!');
+// });
+
+// const startServer = async () => {
+//   try {
+//     connectDB(process.env.MONGODB_URL);
+//     app.listen(9000, () =>
+//       console.log('Server has started on port http://localhost:9000')
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// startServer();
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
@@ -19,6 +52,12 @@ app.get('/', async (req, res) => {
   res.send('Happy Coding!!');
 });
 
+// Serve the service worker with the correct MIME type
+app.get('/serviceWorker.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.sendFile(__dirname + '/serviceWorker.js');
+});
+
 const startServer = async () => {
   try {
     connectDB(process.env.MONGODB_URL);
@@ -31,16 +70,3 @@ const startServer = async () => {
 };
 
 startServer();
-
-// const http = require('http');
-// const PORT = 3000;
-
-// const server = http.createServer((req, res) => {
-//   res.statusCode = 200;
-//   res.setHeader('Content-Type', 'text/plain');
-//   res.end('Hello World!');
-// });
-
-// server.listen(PORT, () => {
-//   console.log(`Server running at http://localhost:${PORT}/`);
-// });
