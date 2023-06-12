c2092ac4a436eb86323af4b881b951a18c2ce281
jinwoo park <elvintern@gmail.com>
2023-06-12 14:51:14 +1200

    PWA

--- index.js
+++ index.js
@@ -1,42 +1,9 @@
-// import express from 'express';
-// import * as dotenv from 'dotenv';
-// import cors from 'cors';
-// import connectDB from './mongodb/connect.js';
-// import quoteRoutes from './routes/quoteRoutes.js';
-// import userRoutes from './routes/userRoutes.js';
-
-// dotenv.config();
-
-// const app = express();
-
-// app.use(cors());
-// app.use(express.json({ limit: '50mb' }));
-
-// app.use('/api/v1/quote', quoteRoutes);
-// app.use('/api/v1/user', userRoutes);
-
-// app.get('/', async (req, res) => {
-//   res.send('Happy Coding!!');
-// });
-
-// const startServer = async () => {
-//   try {
-//     connectDB(process.env.MONGODB_URL);
-//     app.listen(9000, () =>
-//       console.log('Server has started on port http://localhost:9000')
-//     );
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
 import express from 'express';
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 import connectDB from './mongodb/connect.js';
 import quoteRoutes from './routes/quoteRoutes.js';
 import userRoutes from './routes/userRoutes.js';
-import path from 'path';
 
 dotenv.config();
 
@@ -47,11 +14,6 @@ app.use(express.json({ limit: '50mb' }));
 
 app.use('/api/v1/quote', quoteRoutes);
 app.use('/api/v1/user', userRoutes);
-
-app.get('/serviceWorker.js', (req, res) => {
-  res.setHeader('Content-Type', 'application/javascript');
-  res.sendFile(path.join(__dirname, '../client/dist/serviceWorker.js'));
-});
 
 app.get('/', async (req, res) => {
   res.send('Happy Coding!!');
@@ -68,4 +30,42 @@ const startServer = async () => {
   }
 };
 
-startServer();
+// import express from 'express';
+// import * as dotenv from 'dotenv';
+// import cors from 'cors';
+// import connectDB from './mongodb/connect.js';
+// import quoteRoutes from './routes/quoteRoutes.js';
+// import userRoutes from './routes/userRoutes.js';
+// import path from 'path';
+
+// dotenv.config();
+
+// const app = express();
+
+// app.use(cors());
+// app.use(express.json({ limit: '50mb' }));
+
+// app.use('/api/v1/quote', quoteRoutes);
+// app.use('/api/v1/user', userRoutes);
+
+// app.get('/serviceWorker.js', (req, res) => {
+//   res.setHeader('Content-Type', 'application/javascript');
+//   res.sendFile(path.join(__dirname, '../client/dist/serviceWorker.js'));
+// });
+
+// app.get('/', async (req, res) => {
+//   res.send('Happy Coding!!');
+// });
+
+// const startServer = async () => {
+//   try {
+//     connectDB(process.env.MONGODB_URL);
+//     app.listen(9000, () =>
+//       console.log('Server has started on port http://localhost:9000')
+//     );
+//   } catch (error) {
+//     console.log(error);
+//   }
+// };
+
+// startServer();
