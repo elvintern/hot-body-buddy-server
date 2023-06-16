874675acb5b9b1ac2d0159c264462ff45e19c74a
jinwoo park <elvintern@gmail.com>
2023-06-16 17:52:25 +1200

    PWA

--- index.js
+++ index.js
@@ -1,44 +1,9 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './mongodb/connect.js';
-import quoteRoutes from './routes/quoteRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-
-dotenv.config();
-
-const app = express();
-
-app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-
-app.use('/api/v1/quote', quoteRoutes);
-app.use('/api/v1/user', userRoutes);
-
-app.get('/', async (req, res) => {
-  res.send('Happy Coding!!');
-});
-
-const startServer = async () => {
-  try {
-    connectDB(process.env.MONGODB_URL);
-    app.listen(9000, () =>
-      console.log('Server has started on port http://localhost:9000')
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-startServer();
-
 // import express from 'express';
 // import * as dotenv from 'dotenv';
 // import cors from 'cors';
 // import connectDB from './mongodb/connect.js';
 // import quoteRoutes from './routes/quoteRoutes.js';
 // import userRoutes from './routes/userRoutes.js';
-// import path from 'path';
 
 // dotenv.config();
 
@@ -49,11 +14,6 @@ startServer();
 
 // app.use('/api/v1/quote', quoteRoutes);
 // app.use('/api/v1/user', userRoutes);
-
-// app.get('/serviceWorker.js', (req, res) => {
-//   res.setHeader('Content-Type', 'application/javascript');
-//   res.sendFile(path.join(__dirname, '../client/dist/serviceWorker.js'));
-// });
 
 // app.get('/', async (req, res) => {
 //   res.send('Happy Coding!!');
@@ -71,3 +31,43 @@ startServer();
 // };
 
 // startServer();
+
+import express from 'express';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
+import connectDB from './mongodb/connect.js';
+import quoteRoutes from './routes/quoteRoutes.js';
+import userRoutes from './routes/userRoutes.js';
+import path from 'path';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json({ limit: '50mb' }));
+
+app.use('/api/v1/quote', quoteRoutes);
+app.use('/api/v1/user', userRoutes);
+
+app.get('/serviceWorker.js', (req, res) => {
+  res.setHeader('Content-Type', 'application/javascript');
+  res.sendFile(path.join(__dirname, '../client/dist/serviceWorker.js'));
+});
+
+app.get('/', async (req, res) => {
+  res.send('Happy Coding!!');
+});
+
+const startServer = async () => {
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(9000, () =>
+      console.log('Server has started on port http://localhost:9000')
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
